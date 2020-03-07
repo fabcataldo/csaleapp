fc204cde8140a211955914880d2436d16d4ee3d6
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-07 19:35:54 -0300

    Paramertización del componente Login para cuando vienen datos del componente Crear cuenta, si es que el usuario se registró en la app.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { HomePage } from '../home/home';
 import { UserServiceProvider } from '../../providers/user-service';
+import { Storage } from '@ionic/storage';
+import { RegisterPage } from '../register/register';
 
 /**
  * Generated class for the LoginPage page.
@@ -20,30 +22,42 @@ export class LoginPage {
   loginForm: FormGroup;
   user: any;
 
-
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,
-    private UserService: UserServiceProvider) {
+    private UserService: UserServiceProvider, public storage: Storage) {
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(4)])]
-    })
+    })    
+    
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    if(this.navParams.get('email') && this.navParams.get('password') ){
+      this.loginForm.setValue({
+        email: this.navParams.get('email'), 
+        password: this.navParams.get('password')
+      });
+    }
   }
 
   get formFields() { return this.loginForm.controls; }
 
   async onClickSubmit(data){
-    console.log(data)
     await this.UserService.loginUser(data)
     .then( (data)=>{
       this.user=data;
-      console.log(this.user)
-    }); 
-    console.log(this.user)
-    this.navCtrl.push(HomePage);
+      this.storage.set('user',this.user.user);
+      this.storage.set('token',this.user.token);
+
+      this.navCtrl.setRoot(HomePage);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
+
+  goToRegisterPage(){
+    this.navCtrl.push(RegisterPage);
   }
 
 }
