64ffdcbc6a59d1142198032b33fa487c0f72dd1b
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-07 12:06:23 -0300

    Agregado de lógica para el paso de la página del login al home.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Comments } from '../../models/comments';
-import { CommentsServiceProvider } from '../../providers/comments-service/comments-service';
+//import { Comments } from '../../models/comments';
+import { CommentsServiceProvider } from '../../providers/comments-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  comments: any[]=[];
+  comments: any;
+  
 
   constructor(public navCtrl: NavController, public CommentsServiceProvider: CommentsServiceProvider) {
     
   }
 
   ionViewDidLoad(){
-    this.CommentsServiceProvider.getComments().subscribe(
-      (data)=>{
-        this.comments=data;
-      },
-      (error)=>{
-        console.error(error)
-      }
-    );
+    //this.getData();
   }
+
+  /*
+  getData(){
+    this.CommentsServiceProvider.getComments('enviar_token')
+    .then( (data)=>{
+        this.comments=data;
+      });      
+  }
+  */
 }
