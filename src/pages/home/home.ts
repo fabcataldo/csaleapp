c0b901af8d56af3ce5b39600a13d86cf2d0fb56e
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-17 20:48:01 -0300

    Agregado de estilo y esbozo de cómo va a ser el menú hamburguesa.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -7,14 +10,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   comments: any;
+  userLogged: any;
+  token: any;
+  userLoggedName: string;
   
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth, public storage: Storage ) {
     
   }
 
   ionViewDidLoad(){
-
+    console.log('asd')
+    this.getStorageValues();
   }
+
+  ionViewWillEnter(){
+    
+  }
+  async getStorageValues(){
+    this.userLogged = await this.storage.get('userOAuth')
+    this.token = await this.storage.get('token');
+    this.userLoggedName = this.userLogged.name ? this.userLogged.name : null;
+  }
+
+  signOut(): void {
+    try{
+    this.afAuth.auth.signOut().then((result)=>{
+      //elimino lo que haya en el storage, y me voy a
+      this.storage.remove('user').then(()=>{
+
+      })
+      this.storage.remove('token').then(()=>{
+
+      })
+
+      this.navCtrl.setRoot(LoginPage)
+    })
+  }catch(err){
+    console.log(err)
+  }
+   }
 
 }
