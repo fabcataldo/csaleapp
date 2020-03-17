97abd597eaa61c33bdd0b66c5060ce1d094bd23a
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-17 20:51:55 -0300

    Limpieza de código para los métodos del provider de usuarios.

--- src/providers/user-service.ts
+++ src/providers/user-service.ts
@@ -30,7 +30,7 @@ export class UserServiceProvider {
 
   getUser(id, token){
     return new Promise(resolve => {
-      this.http.get<Users>('http://192.168.0.16:3000/api/users/'+id, {
+      this.http.get<Users>('http://192.168.0.78:3000/api/users/'+id, {
         headers: new HttpHeaders().set('Authorization', token)
       })
         .subscribe(res => {
@@ -57,9 +57,8 @@ export class UserServiceProvider {
   }
 
   postUser(user) {
-    user.client = true;
     return new Promise(resolve => {
-      this.http.post('http://192.168.0.30:3000/api/users', user, {})
+      this.http.post('http://192.168.0.78:3000/api/users', user, {})
         .subscribe(res => {
           resolve(res);
         }, (err) => {
@@ -86,7 +85,7 @@ export class UserServiceProvider {
   loginUser(user){
     //en user debo enviar el email y el password
     return new Promise(resolve => {
-      this.http.post('http://192.168.0.30:3000/api/login', user, {})
+      this.http.post('http://192.168.0.78:3000/api/login', user, {})
         .subscribe(res => {
           //devuelve los datos del usuario logeado y el token
           resolve(res);
