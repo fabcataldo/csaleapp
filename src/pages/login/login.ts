1ac39b19158993980eb874f136186c846b3cf312
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-17 20:49:12 -0300

    Implementación de componentes y estilos de cada uno, para los campos del login. Implementación del login con Google y Facebook.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,6 +5,11 @@ import { HomePage } from '../home/home';
 import { UserServiceProvider } from '../../providers/user-service';
 import { Storage } from '@ionic/storage';
 import { RegisterPage } from '../register/register';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Platform } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook/ngx';
 
 /**
  * Generated class for the LoginPage page.
@@ -19,45 +24,161 @@ import { RegisterPage } from '../register/register';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  userOAuth: any;
+  tokenUserOAuth: String;
   loginForm: FormGroup;
   user: any;
+  loggedIn: boolean;
+  FB_APP_ID: number = 326509618306218;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,
-    private UserService: UserServiceProvider, public storage: Storage) {
+    private UserService: UserServiceProvider, public storage: Storage,
+    private afAuth: AngularFireAuth,
+    private gplus: GooglePlus, private platform: Platform,
+    private fb: Facebook
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(4)])]
-    })    
-    
+    })
+
   }
 
   ionViewDidLoad() {
-    if(this.navParams.get('email') && this.navParams.get('password') ){
+    if (this.navParams.get('email') && this.navParams.get('password')) {
       this.loginForm.setValue({
-        email: this.navParams.get('email'), 
+        email: this.navParams.get('email'),
         password: this.navParams.get('password')
       });
     }
   }
-
   get formFields() { return this.loginForm.controls; }
 
-  async onClickSubmit(data){
+  async onClickSubmit(data) {
     await this.UserService.loginUser(data)
-    .then( (data)=>{
-      this.user=data;
-      this.storage.set('user',this.user.user);
-      this.storage.set('token',this.user.token);
+      .then((data) => {
+        this.user = data;
+        this.storage.set('user', this.user.user);
+        this.storage.set('token', this.user.token);
+        this.navCtrl.setRoot(HomePage);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-      this.navCtrl.setRoot(HomePage);
-    })
-    .catch((err)=>{
-      console.log(err);
+  saveOAuthUser() {
+    this.UserService.postUser(this.userOAuth)
+      .then((res: any) => {
+        this.userOAuth = res.user;
+        this.tokenUserOAuth = res.token;
+        this.storage.set('userOAuth', this.userOAuth);
+        this.storage.set('token', this.tokenUserOAuth);
+        this.navCtrl.setRoot(HomePage);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  goToRegisterPage() {
+    this.navCtrl.push(RegisterPage);
+  }
+
+  socialLogin(facebook: boolean) {
+    if (this.platform.is('cordova')) {
+      if (facebook) {
+        this.nativeFacebookLogin();
+      }
+      else {
+        this.nativeGoogleLogin();
+      }
+    } else {
+      if (facebook) {
+        this.webFacebookLogin();
+      }
+      else {
+        this.webGoogleLogin();
+      }
+    }
+  }
+
+  async nativeGoogleLogin(): Promise<void> {
+    try {
+      const gplusUser = await this.gplus.login({
+        'webClientId': '979369541957-fg74ign642oj0dv66jonrmpkp45rj3ka.apps.googleusercontent.com',
+        'offline': true,
+        'scopes': 'profile email'
+      })
+
+      const credential = await this.afAuth.auth.signInWithCredential(
+        firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken)
+      )
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async nativeFacebookLogin() {
+    const permissions = ["public_profile", "email"];
+    this.fb.login(permissions).then(response => {
+      if(response.status === "connected") {
+        console.log('Logged')
+      } else {
+        console.log("Not logged in :(");
+      }
+    }, error => {
+      console.log(error);
     });
   }
 
-  goToRegisterPage(){
-    this.navCtrl.push(RegisterPage);
+  async webFacebookLogin(): Promise<void> {
+    try {
+      let user = null;
+      var newUser = null;
+      const provider = new firebase.auth.FacebookAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider)
+        .then((result) => {
+          user = result.user;
+          result.user.getIdToken().then((token) => {
+            this.tokenUserOAuth = token
+          })
+          newUser = {
+            name: result.user.displayName.split(" ")[0],
+            surname: result.user.displayName.split(" ")[1], email: result.user.email,
+            password: '', role: null, loggedWithOAuth2: true
+          };
+        });
+      this.userOAuth = newUser;
+      this.saveOAuthUser();
+
+    } catch (err) {
+      console.log(err);
+      this.afAuth.auth.signOut()
+    }
+  }
+
+  async webGoogleLogin(): Promise<void> {
+    try {
+      let userAuth = null;
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider)
+        .then((result) => {
+          userAuth = {
+            name: result.user.displayName.split(" ")[0],
+            surname: result.user.displayName.split(" ")[1], email: result.user.email,
+            password: '', role: null, loggedWithOAuth2: true
+          };
+
+          //var credential2 = result.credential as firebase.auth.OAuthCredential       
+          //this.tokenUserOAuth = credential2.accessToken;
+        });
+      this.userOAuth = userAuth
+      this.saveOAuthUser();
+
+    } catch (err) {
+      console.log(err);
+    }
   }
 
 }
