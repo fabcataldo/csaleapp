6495bb229e005798aabe54e3c8b68acb1084f3e5
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-18 20:08:04 -0300

    Creación inicial de la página de Actualizar lugar.

---
+++ src/pages/update-place/update-place.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UpdatePlacePage } from './update-place';
+
+@NgModule({
+  declarations: [
+    UpdatePlacePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UpdatePlacePage),
+  ],
+})
+export class UpdatePlacePageModule {}
