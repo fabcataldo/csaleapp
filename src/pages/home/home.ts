bc5f726549dad757620887a612f4df5ddcd1ec80
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-19 17:29:35 -0300

    Implementación inciial de las opciones del menú hamburguesa.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Storage } from '@ionic/storage';
+import { BenefitsAvailablePage } from '../benefits-available/benefits-available';
+import { PurchasesMadePage } from '../purchases-made/purchases-made';
+import { UpdatePlacePage } from '../update-place/update-place';
 
 @Component({
   selector: 'page-home',
@@ -20,31 +23,49 @@ export class HomePage {
   }
 
   ionViewDidLoad(){
-    console.log('asd')
     this.getStorageValues();
   }
 
-  ionViewWillEnter(){
-    
-  }
   async getStorageValues(){
-    this.userLogged = await this.storage.get('userOAuth')
+    this.userLogged = await this.storage.get('userOAuth') ? await this.storage.get('userOAuth') : await this.storage.get('user')
     this.token = await this.storage.get('token');
     this.userLoggedName = this.userLogged.name ? this.userLogged.name : null;
+  }
+
+  goToBenefitsAvailablePage(){
+    this.navCtrl.push(BenefitsAvailablePage);
+  }
+
+  goToPurchasesMadePage(){
+    this.navCtrl.push(PurchasesMadePage);
+  }
+
+  goToUpdatePlacePage(){
+    this.navCtrl.push(UpdatePlacePage);
+  }
+
+  logOut(){
+    if(this.userLogged.loggedWithOAuth2){
+      this.signOut();
+      this.storage.remove('userOAuth').then(()=>{
+
+      })
+    }
+    else{
+      this.storage.remove('user').then(()=>{
+
+      })  
+    }
+    this.storage.remove('token').then(()=>{
+    })
+
+    this.navCtrl.setRoot(LoginPage)
   }
 
   signOut(): void {
     try{
     this.afAuth.auth.signOut().then((result)=>{
-      //elimino lo que haya en el storage, y me voy a
-      this.storage.remove('user').then(()=>{
-
-      })
-      this.storage.remove('token').then(()=>{
-
-      })
-
-      this.navCtrl.setRoot(LoginPage)
+      console.log(result);
     })
   }catch(err){
     console.log(err)
