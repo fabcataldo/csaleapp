4f7b451db5051ba8d0055fd39f0c8b8473e63c4b
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-23 16:22:38 -0300

    Limpieza de código del modelo Privileges.

--- src/models/privileges.ts
+++ src/models/privileges.ts
@@ -1,12 +1,13 @@
 export class Privileges {
-    _id : string;
-    name : string;
-    description : string;
+    _id: string;
+    name: string;
+    description: string;
 
-    constructor(privilege ? : any) {
-    if(privilege){
-        this._id = privilege._id ? privilege._id : null;
-        this.name = privilege.name ? privilege.name : '';
-        this.description = privilege.description ? privilege.description : '';
+    constructor(privilege?: any) {
+        if (privilege) {
+            this._id = privilege._id ? privilege._id : null;
+            this.name = privilege.name ? privilege.name : '';
+            this.description = privilege.description ? privilege.description : '';
+        }
     }
 }
