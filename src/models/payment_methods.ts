181d55f08aa8311d2b1eda3123673373dcbf1cc6
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-28 21:22:29 -0300

    Modelo Payment_methods: cambio en la implementación para adecuarlo a un documento embebido, según el cambio hecho en el backend.

--- src/models/payment_methods.ts
+++ src/models/payment_methods.ts
@@ -1,11 +1,9 @@
 export class PaymentMethods {
-    _id : string;
     name : string;
     amountPaid : number;
 
     constructor(paymentMethod ? : any) {
         if(paymentMethod){
-            this._id = paymentMethod._id;
             this.name = paymentMethod.name;
             this.amountPaid = paymentMethod.amountPaid;
         }
