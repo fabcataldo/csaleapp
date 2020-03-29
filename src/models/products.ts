eed6f84ee3467915537c1fc1aec72480181e53d3
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-28 21:21:04 -0300

    Modificación del modelo Products. Cambio de lógica en cuanto a que un producto corresponde a una compra realizada, NO a un producto que publicó un lugar.

--- src/models/products.ts
+++ src/models/products.ts
@@ -4,14 +4,14 @@ export class Products {
     _id : string;
     description : string;
     price : number;
-    place : Places;
+    quantity : number;
 
     constructor(product ? : any) {
         if (product) {
             this._id = product._id ? product._id : null;
             this.description = product.description ? product.description : '';
             this.price = product.price ? product.price : 0;
-            this.place = product.place ? product.place : new Places();
+            this.quantity = product.quantity ? product.quantity : 0;
         }
     }
 }
