38d7913a6eb4aad5f234e101d2b65fbf8bb30175
fabcataldo <fabioalbertocataldo@gmail.com>
2020-03-28 21:21:54 -0300

    Modelo Tickets: agregado del dato del total de la compra, y mapeo de las fechas.

--- src/models/tickets.ts
+++ src/models/tickets.ts
@@ -6,21 +6,21 @@ import { PaymentMethods } from './payment_methods';
 export class Tickets {
     _id : string;
     dateOfPurchase : Date;
-    place : Places;
     user : Users;
     products : Products;
     paymentMethods : [PaymentMethods];
     validDateFrom : Date;
     validDateTo : Date;
+    total : Number;
 
     constructor(ticket ? : any) {
         this._id = ticket._id ? ticket._id : null;
-        this.dateOfPurchase = ticket.dateOfPurchase ? ticket.dateOfPurchase : new Date();
-        this.place = ticket.place ? ticket.place : new Places();
+        this.dateOfPurchase = ticket.date_of_purchase ? new Date(ticket.date_of_purchase) : new Date();
         this.user = ticket.user ? ticket.user : new Users();
         this.products = ticket.products ? ticket.products : new Products();
-        this.paymentMethods = ticket.paymentMethods ? ticket.paymentMethods : new PaymentMethods();
-        this.validDateFrom = ticket.validDateFrom ? ticket.validDateFrom : new Date();
-        this.validDateTo = ticket.validDateTo ? ticket.validDateTo : new Date();
+        this.paymentMethods = ticket.payment_methods ? ticket.payment_methods : new PaymentMethods();
+        this.validDateFrom = ticket.valid_date_from ? new Date(ticket.valid_date_from) : new Date();
+        this.validDateTo = ticket.valid_date_to ? new Date(ticket.valid_date_to) : new Date();
+        this.total = ticket.total ? ticket.total : 0;
     }
 }
