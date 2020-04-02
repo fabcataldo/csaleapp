fa58af2112ddd26d95486a577daa5cbb468496ff
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-02 19:47:18 -0300

    Modelo Places: agregado de los atributos latitud y longitud, para verificar el registro de los lugares en la app.

--- src/models/places.ts
+++ src/models/places.ts
@@ -3,11 +3,15 @@ export class Places {
     name : string;
     address : string;
     number_of_people_enabled : number;
+    lat: number;
+    lng: number;
 
    constructor(place ? : any) {
     if(place)
        this._id = place._id ? place._id : null;
        this.name = place.name ? place.name : '';
+       this.lat = place.lat ? place.lat : '';
+       this.lng = place.lng ? place.lng : '';
        this.address = place.address ? place.address : '';
        this.number_of_people_enabled = place.number_of_people_enabled ? place.number_of_people_enabled : 0;
    }
