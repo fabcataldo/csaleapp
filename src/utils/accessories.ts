5960d7b4a8686f1eb8245b096f34ddb689f54f1b
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-05 18:19:49 -0300

    Agregado a la clase Accessories, el método que mapea arreglos de modelos a elección.

--- src/utils/accessories.ts
+++ src/utils/accessories.ts
@@ -1,6 +1,22 @@
+import { Products } from "../models/products";
+import { PaymentMethods } from "../models/payment_methods";
+import { Tickets } from "../models/tickets";
+import { Comments } from "../models/comments";
+
 export class Accessories{
     static formatDate(date){
         var tmp = new Date(date)
         return tmp.getDate()+'/'+(tmp.getMonth()+1)+'/'+tmp.getFullYear()+' '+tmp.getHours()+':'+tmp.getMinutes()+':'+tmp.getSeconds();
+    }
+
+    static mapModelArray(array, model){
+        if(model==='products')
+            return array.map((item)=> new Products(item));
+        if(model==='tickets')
+            return array.map((item)=> new Tickets(item));
+        if(model==='payment_methods')
+            return array.map((item)=> new PaymentMethods(item));
+        if(model==='comments')
+            return array.map((item)=> new Comments(item));
     }
 }
