b28316fbb19d2b231e3f59025cbe2ea48a3e3ec8
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-11 20:41:49 -0300

    Funcionalidad Comprar Productos: se agregó lógica inicial para listar de productos.

--- src/pages/shopping/shopping.ts
+++ src/pages/shopping/shopping.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProductsServiceProvider } from '../../providers/products-service';
+import { Products } from '../../models/products';
 
 /**
  * Generated class for the PurchaseProductsPage page.
@@ -14,8 +16,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'shopping.html',
 })
 export class ShoppingPage {
+  products: Products[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ProductsSrv: ProductsServiceProvider) {
+    this.ProductsSrv.getProducts().subscribe((result)=>{
+      this.products = result;
+    })
   }
 
   ionViewDidLoad() {
