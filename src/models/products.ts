e47f1061955c2c09f1169c18b8a9bb7b15d9c10c
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-13 18:17:44 -0300

    Cambio en los modelos Products y Tickets. En Products se agrega los datos sobre la validez de un producto.

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,16 +1,21 @@
-import { Places } from './places';
+
+import { Accessories } from '../utils/accessories';
 
 export class Products {
     _id : string;
     description : string;
     price : number;
     quantity : number;
+    validDateFrom : string;
+    validDateTo : string;
 
     constructor(product ? : any) {
         if (product) {
             this._id = product._id ? product._id : null;
             this.description = product.description ? product.description : '';
             this.price = product.price ? product.price : 0;
+            this.validDateFrom = product.valid_date_from ? Accessories.formatDate(product.valid_date_from) : Accessories.formatDate(new Date().toISOString());
+            this.validDateTo = product.valid_date_to ? Accessories.formatDate(product.valid_date_to) : Accessories.formatDate(new Date().toISOString());
         }
     }
 }
--- src/models/tickets.ts
+++ src/models/tickets.ts
@@ -4,22 +4,20 @@ import { Accessories } from '../utils/accessories';
 import { PurchasedProducts } from './purchased_products';
 
 export class Tickets {
-    _id : string;
-    dateOfPurchase : string;
-    user : Users;
-    paymentMethods : [PaymentMethods];
-    validDateFrom : string;
-    validDateTo : string;
-    total : Number;
-    purchasedProducts: PurchasedProducts[];
+    _id : string = "0";
+    dateOfPurchase : string = "";
+    user : Users = null;
+    paymentMethods : PaymentMethods[] = [];
+    total : number = 0;
+    purchasedProducts: PurchasedProducts[] = [];
 
     constructor(ticket ? : any) {
-        this._id = ticket._id ? ticket._id : null;
-        this.dateOfPurchase = ticket.date_of_purchase ? Accessories.formatDate(ticket.date_of_purchase) : Accessories.formatDate(new Date().toISOString());
-        this.paymentMethods = ticket.payment_methods ? Accessories.mapModelArray(ticket.payment_methods, 'payment_methods') : null;
-        this.validDateFrom = ticket.valid_date_from ? Accessories.formatDate(ticket.valid_date_from) : Accessories.formatDate(new Date().toISOString());
-        this.validDateTo = ticket.valid_date_to ? Accessories.formatDate(ticket.valid_date_to) : Accessories.formatDate(new Date().toISOString());
-        this.total = ticket.total ? ticket.total : 0;
-        this.purchasedProducts = ticket.purchased_products ? Accessories.mapModelArray(ticket.purchased_products, 'purchased_products') : null;
+        if(ticket){
+            this._id = ticket._id ? ticket._id : null;
+            this.dateOfPurchase = ticket.date_of_purchase ? Accessories.formatDate(ticket.date_of_purchase) : Accessories.formatDate(new Date().toISOString());
+            this.paymentMethods = ticket.payment_methods ? Accessories.mapModelArray(ticket.payment_methods, 'payment_methods') : null;
+            this.total = ticket.total ? ticket.total : 0;
+            this.purchasedProducts = ticket.purchased_products ? Accessories.mapModelArray(ticket.purchased_products, 'purchased_products') : null;
+        }
     }
 }
