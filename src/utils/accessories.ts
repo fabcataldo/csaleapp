9bdb4b2308df5dc64e082661e9b1c780e5939e0f
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-25 17:50:03 -0300

    Accessories: implementación de validador de números de tarjeta, y de método auxiliar que genera strings aleatorios para hacer nombres de comprobantes o tickets únicos.

--- src/utils/accessories.ts
+++ src/utils/accessories.ts
@@ -3,6 +3,7 @@ import { PaymentMethods } from "../models/payment_methods";
 import { Tickets } from "../models/tickets";
 import { Comments } from "../models/comments";
 import { PurchasedProducts } from "../models/purchased_products";
+import { AbstractControl } from "@angular/forms";
 
 export class Accessories{
     static formatDate(date){
@@ -30,5 +31,45 @@ export class Accessories{
 
     static capitalizeFirstChar(string){
         return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static cardValidator(control: AbstractControl): {[key: string]: boolean} | null{
+        let cards = {
+            'mc': '5[1-5][0-9]{14}',
+            'ec': '5[1-5][0-9]{14}',
+            'vi': '4(?:[0-9]{12}|[0-9]{15})',
+            'ax': '3[47][0-9]{13}',
+            'dc': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+            'bl': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+            'di': '6011[0-9]{12}',
+            'jcb': '(?:3[0-9]{15}|(2131|1800)[0-9]{11})',
+            'er': '2(?:014|149)[0-9]{11}'
+        };
+        let value = String(control.value).replace(/[- ]/g, ''); //ignore dashes and whitespaces
+
+        var cardinfo = cards, results = [];
+        /*
+        if(ccType){
+                var expr = '^' + cardinfo[ccType.toLowerCase()] + '$';
+                return expr ? !!value.match(expr) : false; // boolean
+        }
+         */
+
+        for (var p in cardinfo) {
+            if (value.match('^' + cardinfo[p] + '$')) {
+                results.push(p);
+            }
+        }
+        return results.length ? { validCard: true } : null;
+    };
+
+    static makeRandomString(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
     }
 }
