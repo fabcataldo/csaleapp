08a007a758843022a4007d192dc5bf2ac8db6aa8
fabcataldo <fabioalbertocataldo@gmail.com>
2020-04-25 17:45:47 -0300

    Agregado de datos de tarjeta en el modelo de métodos de pago de un ticket,

--- src/models/payment_methods.ts
+++ src/models/payment_methods.ts
@@ -1,15 +1,18 @@
 import { AvailablePaymentMethods } from "./available_payment_methods";
+import { Cards } from "./cards";
 
 export class PaymentMethods {
+    id: string;
     amountPaid: number;
     paymentMethod : AvailablePaymentMethods;
-    creditCard: string;
+    card: Cards;
 
     constructor(paymentMethod ? : any) {
         if(paymentMethod){
-            this.paymentMethod = paymentMethod.payment_method;
-            this.creditCard = paymentMethod.credit_card;
-            this.amountPaid = paymentMethod.amount_paid;
+            this.id = paymentMethod.id ? paymentMethod.id : paymentMethod._id ? paymentMethod._id : null;
+            this.paymentMethod = paymentMethod.paymentMethod ? paymentMethod.paymentMethod : paymentMethod.payment_method ? paymentMethod.payment_method : null;
+            this.card = paymentMethod.card ? paymentMethod.card : null;
+            this.amountPaid = paymentMethod.amountPaid ? paymentMethod.amountPaid : paymentMethod.amount_paid ? paymentMethod.amount_paid : 0;
         }
     }
 }
