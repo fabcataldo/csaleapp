0ba5dcfd081e633b3ad89a8a9545ba0ab1975f54
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-25 21:05:33 -0300

    Cambio de nombres de variables en el Modelo de PaymentMethods.

--- src/models/payment_methods.ts
+++ src/models/payment_methods.ts
@@ -3,16 +3,16 @@ import { Cards } from "./cards";
 
 export class PaymentMethods {
     id: string;
-    amountPaid: number;
-    paymentMethod : AvailablePaymentMethods;
+    amount_paid: number;
+    payment_method : AvailablePaymentMethods;
     card: Cards;
 
     constructor(paymentMethod ? : any) {
         if(paymentMethod){
             this.id = paymentMethod.id ? paymentMethod.id : paymentMethod._id ? paymentMethod._id : null;
-            this.paymentMethod = paymentMethod.paymentMethod ? paymentMethod.paymentMethod : paymentMethod.payment_method ? paymentMethod.payment_method : null;
+            this.payment_method = paymentMethod.payment_method ? paymentMethod.payment_method : null;
             this.card = paymentMethod.card ? paymentMethod.card : null;
-            this.amountPaid = paymentMethod.amountPaid ? paymentMethod.amountPaid : paymentMethod.amount_paid ? paymentMethod.amount_paid : 0;
+            this.amount_paid = paymentMethod.amount_paid ? paymentMethod.amount_paid : 0;
         }
     }
 }
