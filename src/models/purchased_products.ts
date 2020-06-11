4dc530b40a57d69c9f3044579b481d4675690131
fabcataldo <fabioalbertocataldo@gmail.com>
2020-06-11 19:40:22 -0300

    Cambio nombre en el modelo PurchasedProducts, por el que viene ya de las consultas de ésta entidad a la bd.

--- src/models/purchased_products.ts
+++ src/models/purchased_products.ts
@@ -2,13 +2,13 @@ import { Products } from "./products";
 import { Accessories } from '../utils/accessories';
 
 export class PurchasedProducts {
-    id: number;
+    _id: number;
     product : Products;
     quantity: number;
 
     constructor(purchasedProducts ? : any) {
         if(purchasedProducts){
-            this.id = purchasedProducts._id ? purchasedProducts._id : null;
+            this._id = purchasedProducts._id ? purchasedProducts._id : null;
             this.product = Accessories.mapModelObject(purchasedProducts.product, 'products');
             this.quantity = purchasedProducts.quantity ? purchasedProducts.quantity : 0;
         }
