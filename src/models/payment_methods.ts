fe8309f69757e66d6e2d1fb861ea2b96025cf885
fabcataldo <fabioalbertocataldo@gmail.com>
2020-06-11 19:39:46 -0300

    Limpio código del modelo Payment_Methods, debido a que eliminé el modelo Cards

--- src/models/payment_methods.ts
+++ src/models/payment_methods.ts
@@ -1,17 +1,14 @@
 import { AvailablePaymentMethods } from "./available_payment_methods";
-import { Cards } from "./cards";
 
 export class PaymentMethods {
-    id: string;
+    _id: string;
     amount_paid: number;
     payment_method : AvailablePaymentMethods;
-    card: Cards;
 
     constructor(paymentMethod ? : any) {
         if(paymentMethod){
-            this.id = paymentMethod.id ? paymentMethod.id : paymentMethod._id ? paymentMethod._id : null;
+            this._id = paymentMethod._id ? paymentMethod._id : null;
             this.payment_method = paymentMethod.payment_method ? paymentMethod.payment_method : null;
-            this.card = paymentMethod.card ? paymentMethod.card : null;
             this.amount_paid = paymentMethod.amount_paid ? paymentMethod.amount_paid : 0;
         }
     }
