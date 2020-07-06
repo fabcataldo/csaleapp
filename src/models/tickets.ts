c08d8474a21d1715a653d0910613ebbcd76ab93e
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-06 20:43:23 -0300

    Agrego al modelo Tickets, un atributo para el código único.  Limpio código.

--- src/models/tickets.ts
+++ src/models/tickets.ts
@@ -1,4 +1,3 @@
-import { Users } from './users';
 import { PaymentMethods } from './payment_methods';
 import { Accessories } from '../utils/accessories';
 import { PurchasedProducts } from './purchased_products';
@@ -6,10 +5,10 @@ import { PurchasedProducts } from './purchased_products';
 export class Tickets {
     _id : string = null;
     date_of_purchase : string = "";
-    user : Users = null;
     payment_methods : PaymentMethods;
     total : number = 0;
     purchased_products: PurchasedProducts[] = [];
+    unique_code: number = 0;
 
     constructor(ticket ? : any) {
         if(ticket){
@@ -18,6 +17,7 @@ export class Tickets {
             this.payment_methods = ticket.payment_methods ? Accessories.mapModelObject(ticket.payment_methods, 'payment_methods') : null;
             this.total = ticket.total ? ticket.total : 0;
             this.purchased_products = ticket.purchased_products ? Accessories.mapModelArray(ticket.purchased_products, 'purchased_products') : null;
+            this.unique_code = ticket.unique_code ? ticket.unique_code : 0;
         }
     }
 }
