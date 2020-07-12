041074f5d3744edd133f1a329c62958f6da27f4f
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-12 20:51:16 -0300

    Arreglo validador de número de tarjetas.

--- src/utils/accessories.ts
+++ src/utils/accessories.ts
@@ -42,33 +42,27 @@ export class Accessories{
     }
 
     //validador de números de tarjeta
-    static cardValidator(control: AbstractControl): {[key: string]: boolean} | null{
-        let cards = {
-            'mc': '5[1-5][0-9]{14}',
-            'ec': '5[1-5][0-9]{14}',
-            'vi': '4(?:[0-9]{12}|[0-9]{15})',
-            'ax': '3[47][0-9]{13}',
-            'dc': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
-            'bl': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
-            'di': '6011[0-9]{12}',
-            'jcb': '(?:3[0-9]{15}|(2131|1800)[0-9]{11})',
-            'er': '2(?:014|149)[0-9]{11}'
-        };
-        let value = String(control.value).replace(/[- ]/g, ''); //ignore dashes and whitespaces
-
-        var cardinfo = cards, results = [];
-        /*
-        if(ccType){
-                var expr = '^' + cardinfo[ccType.toLowerCase()] + '$';
-                return expr ? !!value.match(expr) : false; // boolean
-        }
-         */
-
-        for (var p in cardinfo) {
-            if (value.match('^' + cardinfo[p] + '$')) {
-                results.push(p);
+    static cardValidator(control: AbstractControl){
+             let cards = {
+                'mc': '5[1-5][0-9]{14}',
+                'ec': '5[1-5][0-9]{14}',
+                'vi': '4(?:[0-9]{12}|[0-9]{15})',
+                'ax': '3[47][0-9]{13}',
+                'dc': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+                'bl': '3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+                'di': '6011[0-9]{12}',
+                'jcb': '(?:3[0-9]{15}|(2131|1800)[0-9]{11})',
+                'er': '2(?:014|149)[0-9]{11}'
+            };
+            let value = String(control.value).replace(/[- ]/g, ''); //ignore dashes and whitespaces
+            var cardinfo = cards, results = [];
+    
+            for (var p in cardinfo) {
+                if (value.match('^' + cardinfo[p] + '$')) {
+                    results.push(p);
+                }
             }
-        }
-        return results.length ? { validCard: true } : null;
-    };
+            return results.length > 0 ? (null) : ({ cardValidator: true });
+    }
+
 }
