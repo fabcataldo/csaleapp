cfdb5a17f00c7afe5066c24e9a59821398b1297b
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-25 16:31:47 -0300

    Agrego URL del srv de heroku

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -2,7 +2,10 @@
 
 export class Global{
     static URL_API='/api';
-    static SRV = 'http://192.168.1.161:3000';
+    //static SRV = 'http://192.168.1.161:3000';
+    //static SRV = "http://192.168.1.145:3000";
+    static SRV = "https://csalesrv.herokuapp.com";
+
     static USER_TOKEN = null;
     static MERCADO_PAGO_PUBLIC_KEY = "TEST-29ec7fdc-602d-4241-bb97-e94a7dfa7b11";
 }
